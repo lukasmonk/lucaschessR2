88125044911b025dee380597b70d654401362808
lukasmonk <lukasmonk@gmail.com>
2023-06-25 19:43:25 +0200

    Version R2.08a

--- bin/Code/__init__.py
+++ bin/Code/__init__.py
@@ -94,7 +94,7 @@ def relative_root(path):
 
 
 BASE_VERSION = "B"  # Para el control de updates que necesitan reinstalar entero
-VERSION = "R 2.08"
+VERSION = "R 2.08a"
 DEBUG = False
 DEBUG_ENGINES = False
 
