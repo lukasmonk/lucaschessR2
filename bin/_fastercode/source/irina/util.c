6dd414e55191f867e57102edadacb8aa8ca22d50
jlrdh <69487096+jlrdh@users.noreply.github.com>
2023-09-07 22:53:05 +0200

    Update util.c
    This change is one of the changes that enabled the fastercode to compile on the ARMV8 architecture

--- bin/_fastercode/source/irina/util.c
+++ bin/_fastercode/source/irina/util.c
@@ -18,8 +18,12 @@ int is_bmi2()
 #include <stdint.h>
 int is_bmi2()
 {
+    #ifdef __aarch64__
+    return O;
+    #else
     __builtin_cpu_init ();
     return __builtin_cpu_supports("bmi2");
+    #endif
 }
 #endif
 
